6c99d95e9b32376594321bbf665740e22fbc1ead
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-03 20:00:26 -0600

    Add fizz, buzz, super functionality and tests

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ function addNumbers(num1, num2){
   return num1 + num2;
 }
 
+function fizzBuzz(number){
+  if( number % 3 === 0 ){
+    return 'Fizz'
+  } else if ( number % 5 === 0 ){
+    return 'Buzz'
+  } else if ( number % 7 === 0 ){
+    return 'Super'
+  }
+}
+
 
 module.exports = {
   sayHello: sayHello,
-  addNumbers: addNumbers
+  addNumbers: addNumbers,
+  fizzBuzz: fizzBuzz
 }
--- test/appTest.js
+++ test/appTest.js
@@ -1,31 +1,55 @@
-const assert   = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNums  = require('../app').addNumbers;
+const assert    = require('chai').assert;
+const sayHello  = require('../app').sayHello;
+const addNums   = require('../app').addNumbers;
+const fizzBuzz  = require('../app').fizzBuzz;
+
 
 describe('App', function(){
-  it('app should return hello', function(){
-    var result = sayHello();
-    assert.equal(result, 'Hello, there Cutie pie! One of us is in deep trouble.');
+  context('Initially learning unit testing', function(){
+    it('app should return hello', function(){
+      var result = sayHello();
+      assert.equal(result, 'Hello, there Cutie pie! One of us is in deep trouble.');
+    })
+
+    it('app should return a string', function(){
+      var result = sayHello();
+      assert.typeOf(result, 'string');
+    })
+
+    it('addNumbers should add two values', function(){
+      var result = addNums(5,5);
+      assert.equal(result, 10)
+    })
+
+    it('addNumbers is greater than 5', function(){
+      var result = addNums(2,4)
+      assert.isAbove(result, 5)
+      assert.equal(result, 6)
+    })
+
+    it('addNumbers returns a number', function(){
+      var result = addNums(5,5);
+      assert.typeOf(result, 'number')
+    })
   })
 
-  it('app should return a string', function(){
-    var result = sayHello();
-    assert.typeOf(result, 'string');
-  })
+  context('fizzBuzz', function(){
+    it('returns Fizz if divisible by 3', function(){
+      var result = fizzBuzz(6);
+      assert.equal(result, 'Fizz')
+      assert.typeOf(result, 'string')
+    })
 
-  it('addNumbers should add two values', function(){
-    var result = addNums(5,5);
-    assert.equal(result, 10)
-  })
+    it('returns Buzz if divisible by 5', function(){
+      var result = fizzBuzz(10);
+      assert.equal(result, 'Buzz')
+      assert.typeOf(result, 'string')
+    })
 
-  it('addNumbers is greater than 5', function(){
-    var result = addNums(2,4)
-    assert.isAbove(result, 5)
-    assert.equal(result, 6)
-  })
-
-  it('addNumbers returns a number', function(){
-    var result = addNums(5,5);
-    assert.typeOf(result, 'number')
+    it('returns Super if divisible by 7', function(){
+      var result = fizzBuzz(14);
+      assert.equal(result, 'Super')
+      assert.typeOf(result, 'string')
+    })
   })
 });
