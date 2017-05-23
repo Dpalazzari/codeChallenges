308ae0a9cd7453b9f83160b271f22096e4ea6b32
Drew Palazzari <drewpalazzari@hotmail.com>
2017-05-22 19:49:04 -0600

    Write easy mode method

--- Ruby/test/reverse_string_test.rb
+++ Ruby/test/reverse_string_test.rb
@@ -12,4 +12,10 @@ class ReverseStringTest < Minitest::Test
     assert @reverse
   end
 
+  def test_it_reverses_a_string
+    phrase = 'babooshka'
+    result = @reverse.easy_method(phrase)
+    assert_equal 'akhsoobab', result
+  end
+
 end
