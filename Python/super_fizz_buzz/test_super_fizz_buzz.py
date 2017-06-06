4a609c05fcc20d8cb4427be22cffc7c8008927ee
Drew Palazzari <drewpalazzari@hotmail.com>
2017-06-06 16:56:26 -0600

    Write initial test file and first test

---
+++ Python/super_fizz_buzz/test_super_fizz_buzz.py
@@ -0,0 +1,15 @@
+import unittest
+from super_fizz_buzz import SuperFizzBuzz
+
+class SuperFizzBuzzTest(unittest.TestCase):
+
+  def setUp(self):
+    self.sfb = SuperFizzBuzz()
+
+  def test_it_returns_super_when_divisible_by_7(self):
+    num = 7
+    result = self.sfb.sequence(num)
+    self.assertEqual(result, 'Super')
+unittest.main()
+
+  
