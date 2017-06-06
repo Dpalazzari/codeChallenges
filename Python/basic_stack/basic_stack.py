9e81e6833cf39921831b3970e4fdcea0176d9908
Drew Palazzari <drewpalazzari@hotmail.com>
2017-06-05 20:36:09 -0600

    Solve python push using append method

--- Python/basic_stack/basic_stack.py
+++ Python/basic_stack/basic_stack.py
@@ -5,3 +5,6 @@ class BasicStack(object):
   def push_to_array(self, arr, num):
     return list(chain.from_iterable([arr, [num]]))
     
+  def push_to_array_with_append(self, arr, num):
+    arr.append(num)
+    return arr
--- Python/basic_stack/test_basic_stack.py
+++ Python/basic_stack/test_basic_stack.py
@@ -7,10 +7,16 @@ class BasicStackTestCase(unittest.TestCase):
   def setUp(self):
     self.basic = BasicStack()
 
-  def test_push_to_array(self):
+  def test_push_to_array_without_append(self):
     array  = [1,2,3,4]
     number = 5
     result = self.basic.push_to_array(array, number)
+    self.assertEqual(result, [1,2,3,4,5])
+
+  def test_push_to_array_with_append(self):
+    array  = [1,2,3,4]
+    number = 5
+    result = self.basic.push_to_array_with_append(array, number)
     self.assertEqual(result, [1,2,3,4,5])
 
 unittest.main()
