d86de653572a793b5b9b9d17a96805883bd21312
Drew Palazzari <drewpalazzari@hotmail.com>
2017-06-07 20:27:06 -0600

    Rename folder tree
    Also broke scrabble board out into its own object.

---
+++ Python/colls/colls.py
@@ -0,0 +1,17 @@
+# Should be called 'collections' but that is a taken buzzword in Python
+from IPython import embed
+from scrabble_board import Scrabble
+
+class Colls:
+
+  def __init__(self):
+    self.states         = {"Oregon": "OR", "Alabama": "AL", "New Jersey": "NJ", "Colorado": "CO"}
+    self.capitals       = {"OR": "Salem", "AL": "Montgomery", "NJ": "Trenton", "CO": "Denver"}
+    self.scrabble_board = Scrabble().board
+
+  def find_capital(self, state):
+    if state in self.states.keys():
+      capital = self.capitals[self.states[state]]
+    if state not in self.states.keys():
+      capital = "Unknown"
+    return capital
