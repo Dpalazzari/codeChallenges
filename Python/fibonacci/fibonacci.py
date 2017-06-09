d216deb26e3c6a1c0e14a4042aecbf3ca6ab0c4d
Drew Palazzari <drewpalazzari@hotmail.com>
2017-06-08 18:10:58 -0600

    Build recursive fib method

--- Python/fibonacci/fibonacci.py
+++ Python/fibonacci/fibonacci.py
@@ -2,4 +2,11 @@ from IPython import embed
 
 class Fibonacci:
 
-  
+  def sequence(self, num):
+    if num <= 2:
+      return [0 ,1]
+    else:
+      fib_seq = self.sequence(num-1)
+      next_fib_num = fib_seq[-1] + fib_seq[-2]
+      fib_seq.append(next_fib_num)
+      return fib_seq
