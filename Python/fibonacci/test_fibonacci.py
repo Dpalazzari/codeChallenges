ca3ef85743173da347604ba06ba2a5bf1fd183aa
Drew Palazzari <drewpalazzari@hotmail.com>
2017-06-08 18:12:38 -0600

    Method complete. all tests pass.

--- Python/fibonacci/test_fibonacci.py
+++ Python/fibonacci/test_fibonacci.py
@@ -21,4 +21,9 @@ class FibonacciTestCase(unittest.TestCase):
     result = self.fibonacci.sequence(num)
     self.assertEqual(result, [0,1,1,2])
 
+  def test_it_returns_the_first_fifteen_numbers_of_the_fib_sequence(self):
+    num = 15
+    result = self.fibonacci.sequence(num)
+    self.assertEqual(result, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+
 unittest.main()
